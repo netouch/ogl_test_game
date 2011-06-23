c8aac64f6e9381ec6110002a387f1102a49966e4
askoropadsky <askoropadsky@ukrtelecom.ua>
2011-06-23 16:41:57 +0300

    corrected logging

--- test_android/src/test/com/GameView.java
+++ test_android/src/test/com/GameView.java
@@ -22,6 +22,7 @@ public class GameView extends GLSurfaceView {
 				gameOpenGlRenderer.pl.rx -= (event.getY()-event.getHistoricalY(1))/100;
 			//gameOpenGlRenderer.pl.rx += -(gameOpenGlRenderer.screenHeight/2- event.getY())/30;
 			//gameOpenGlRenderer.pl.ry += -(gameOpenGlRenderer.screenWidth/2- event.getX())/30;
+			printSamples(event);
 			break;
 		
 		case MotionEvent.ACTION_UP:
@@ -40,16 +41,17 @@ public class GameView extends GLSurfaceView {
 	private void printSamples(MotionEvent ev) {
 	     final int historySize = ev.getHistorySize();
 	     final int pointerCount = ev.getPointerCount();
+	     
 	     for (int h = 0; h < historySize; h++) {
-	    	 android.util.Log.d("OGL",String.format("At time %d:", (int)ev.getHistoricalEventTime(h)));
+	    	 android.util.Log.d("OGL",String.format("At time %d:  History size:%d", (int)ev.getHistoricalEventTime(h), historySize));
 	         for (int p = 0; p < pointerCount; p++) {
-	        	 android.util.Log.d("OGL",String.format("  pointer %d: (X%f Y%f)",
+	        	 android.util.Log.d("OGL",String.format("History  pointer %d: (X%f Y%f)",
 	                 (int)ev.getPointerId(p), (float)ev.getHistoricalX(p, h), (float)ev.getHistoricalY(p, h)));
 	         }
 	     }
 	     android.util.Log.d("OGL",String.format("At time %d:", (int)ev.getEventTime()));
 	     for (int p = 0; p < pointerCount; p++) {
-	    	 android.util.Log.d("OGL",String.format("  pointer %d: (X%f Y%f)",
+	    	 android.util.Log.d("OGL",String.format("Current  pointer %d: (X%f Y%f)",
 	             (int)ev.getPointerId(p), (float)ev.getX(p), (float)ev.getY(p)));
 	     }
 	 }
