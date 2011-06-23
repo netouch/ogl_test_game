c8d8323a3e8a217d867840e849860dd53156d540
askoropadsky <askoropadsky@ukrtelecom.ua>
2011-06-23 16:05:21 +0300

    logging added

--- test_android/src/test/com/GameView.java
+++ test_android/src/test/com/GameView.java
@@ -18,21 +18,39 @@ public class GameView extends GLSurfaceView {
 	public boolean onTouchEvent(MotionEvent event){
 		switch(event.getAction()){
 		case MotionEvent.ACTION_MOVE :
-			//if(event.getHistoricalY(0)!=0)
-				//gameOpenGlRenderer.pl.y -= (event.getY()-event.getHistoricalY(0))/100;
-			gameOpenGlRenderer.pl.rx += -(gameOpenGlRenderer.screenHeight/2- event.getY())/10;
-			gameOpenGlRenderer.pl.ry += -(gameOpenGlRenderer.screenWidth/2- event.getX())/10;
+			if(event.getHistoricalY(0)!=0)
+				gameOpenGlRenderer.pl.rx -= (event.getY()-event.getHistoricalY(1))/100;
+			//gameOpenGlRenderer.pl.rx += -(gameOpenGlRenderer.screenHeight/2- event.getY())/30;
+			//gameOpenGlRenderer.pl.ry += -(gameOpenGlRenderer.screenWidth/2- event.getX())/30;
 			break;
 		
 		case MotionEvent.ACTION_UP:
 			//gameOpenGlRenderer.pl.rx=0;
-			System.out.printf("ACTION_UP at: %f | %f", event.getX(),event.getY());
-
+			//System.out.printf("ACTION_UP at: %f | %f", event.getX(),event.getY());
+			//android.util.Log.d("OGL",String.format("asdasdds X:%d Y:%d", (int)event.getX(), (int)event.getY()) );
+			printSamples(event);
 			break;
 		//case MotionEvent.ACTION_DOWN:
 		//	gameOpenGlRenderer.pl.rx = event.getX()/10;
 		//	break; 
-	}
+		}
 		return true;
 	}
+	
+	private void printSamples(MotionEvent ev) {
+	     final int historySize = ev.getHistorySize();
+	     final int pointerCount = ev.getPointerCount();
+	     for (int h = 0; h < historySize; h++) {
+	    	 android.util.Log.d("OGL",String.format("At time %d:", (int)ev.getHistoricalEventTime(h)));
+	         for (int p = 0; p < pointerCount; p++) {
+	        	 android.util.Log.d("OGL",String.format("  pointer %d: (X%f Y%f)",
+	                 (int)ev.getPointerId(p), (float)ev.getHistoricalX(p, h), (float)ev.getHistoricalY(p, h)));
+	         }
+	     }
+	     android.util.Log.d("OGL",String.format("At time %d:", (int)ev.getEventTime()));
+	     for (int p = 0; p < pointerCount; p++) {
+	    	 android.util.Log.d("OGL",String.format("  pointer %d: (X%f Y%f)",
+	             (int)ev.getPointerId(p), (float)ev.getX(p), (float)ev.getY(p)));
+	     }
+	 }
 }
