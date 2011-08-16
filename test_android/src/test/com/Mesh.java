1ae45a067b159d44822b06004b2ae48af0d5fa5b
askoropadsky <askoropadsky@ukrtelecom.ua>
2011-08-16 17:00:57 +0300

    finish add texture support

--- test_android/src/test/com/Mesh.java
+++ test_android/src/test/com/Mesh.java
@@ -1,6 +1,6 @@
 package test.com;
 
-import java.net.ContentHandler;
+//import java.net.ContentHandler;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -9,8 +9,9 @@ import java.nio.ShortBuffer;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.content.Context;
+//import android.graphics.BitmapFactory;
+import android.opengl.GLUtils;
+//import android.content.Context;
 
 public class Mesh {
 	private FloatBuffer verticesBuffer = null;
@@ -19,9 +20,11 @@ public class Mesh {
 	
 	private float[] rgba = new float[]{1.0f , 1.0f , 1.0f , 1.0f};
 	private FloatBuffer colorBuffer = null;
-	
-	Bitmap bitmap=null;
-	int textures[]=null;
+	//members for texture
+	private FloatBuffer mUVTextureBuffer=null;
+	private int mTextureId = -1;
+	private Bitmap mBitmap=null;
+	private boolean mShouldLoadTexture = false;
 	
 	public float x = 0.0f;
 	public float y = 0.0f;
@@ -31,15 +34,34 @@ public class Mesh {
 	public float ry = 0.0f;
 	public float rz = 0.0f;
 	
+	protected void setTextureCoordinates(float textureCoordinates[]){
+		ByteBuffer byteBuff = ByteBuffer.allocateDirect(textureCoordinates.length*4);
+		byteBuff.order(ByteOrder.nativeOrder());
+		mUVTextureBuffer = byteBuff.asFloatBuffer();
+		mUVTextureBuffer.put(textureCoordinates);
+		mUVTextureBuffer.position(0);
+	}
+	
 	public void loadBitmap(Bitmap btmp){
 		//bitmap = BitmapFactory.decodeResource(Context.getResources(), R.drawable.icon);
-		bitmap = btmp;
+		mBitmap = btmp;
+		mShouldLoadTexture = true;
 	}
 	
 	public void loadTexture(GL10 gl){
-		textures = new int[1];
+		int textures[] = new int[1];
 		gl.glGenTextures(1, textures, 0);
+		mTextureId = textures[0];
+		gl.glBindTexture(GL10.GL_TEXTURE_2D, mTextureId);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
+		gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
 		
+		/* posible parameters
+		   	gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S,GL10.GL_CLAMP_TO_EDGE);
+			gl.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T,GL10.GL_REPEAT);
+		*/
+		
+		GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, mBitmap, 0);
 	}
 	
 	
@@ -56,16 +78,29 @@ public class Mesh {
 			// Specifies the location and data format of an array of vertex
 			// coordinates to use when rendering.
 			gl.glVertexPointer(3, GL10.GL_FLOAT, 0, verticesBuffer);
-		        // Set flat color
-		        gl.glColor4f(rgba[0], rgba[1], rgba[2], rgba[3]);
-		        // Smooth color
-		        if ( colorBuffer != null ) {
-		            // Enable the color array buffer to be used during rendering.
-		            gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
-		            // Point out the where the color buffer is.
-		            gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);
-		        }
-		        
+		    // Set flat color
+		    gl.glColor4f(rgba[0], rgba[1], rgba[2], rgba[3]);
+		    // Smooth color
+		    if(colorBuffer != null){
+		        // Enable the color array buffer to be used during rendering.
+		        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+		        // Point out the where the color buffer is.
+		        gl.glColorPointer(4, GL10.GL_FLOAT, 0, colorBuffer);
+		    }
+		    
+		    if(mShouldLoadTexture){
+		    	loadTexture(gl);
+		    	mShouldLoadTexture = false;
+		    }
+		    
+		    if(mTextureId != -1 && mUVTextureBuffer != null){
+		    	gl.glEnable(GL10.GL_TEXTURE_2D);
+		    	gl.glEnableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+		    	
+		    	gl.glTexCoordPointer(2, GL10.GL_FLOAT, 0, mUVTextureBuffer);
+		    	gl.glBindTexture(GL10.GL_TEXTURE_2D, mTextureId);
+		    }
+		    
 		    gl.glPushMatrix();
 		    gl.glTranslatef(x, y, z);
 		    gl.glRotatef(rx, 1, 0, 0);
@@ -78,6 +113,10 @@ public class Mesh {
 			gl.glPopMatrix();
 			// Disable the vertices buffer.
 			gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+			
+			if(mTextureId != -1 && mUVTextureBuffer != null){
+				gl.glDisableClientState(GL10.GL_TEXTURE_COORD_ARRAY);
+			}
 			// Disable face culling.
 			gl.glDisable(GL10.GL_CULL_FACE);
 	    
